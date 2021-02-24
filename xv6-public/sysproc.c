f7298d19060fa7811f6fe86f6a055e073cfb9a96
andruixxd31 <andruix93@gmail.com>
2021-02-24 02:44:42 +0000

    made reboot and shutdown methods

--- xv6-public/sysproc.c
+++ xv6-public/sysproc.c
@@ -89,3 +89,15 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+//shutdown machine
+int sys_shutdown(void){
+	outw(0x604, 0x2000);
+	return 0;
+}
+
+//reboot machine
+int sys_reboot(void){
+    outb(0xFE, 0x64);
+	return 0;
+}
